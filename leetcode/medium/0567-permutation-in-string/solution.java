c7739806c8ba1c38ee62b149f112a4b5d072c39a
Bhavana Chennu <bhavanachennu61@gmail.com>
2026-09-22 21:28:20 +0530

    ✅ Permutation in String | Medium | Java | Runtime: 7 ms (66.29%) | Memory: 43.8 MB | 2026-09-22

---
+++ leetcode/medium/0567-permutation-in-string/solution.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int n1 = s1.length();
+        int n2 = s2.length();
+        if (n1 > n2) {
+            return false;
+        }
+        int[] count1 = new int[26];
+        int[] count2 = new int[26];
+        for (int i = 0; i < n1; i++) {
+            count1[s1.charAt(i) - 'a']++;
+            count2[s2.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < n2 - n1; i++) {
+            if (Arrays.equals(count1, count2)) {
+                return true;
+            }
+            count2[s2.charAt(i + n1) - 'a']++;
+            count2[s2.charAt(i) - 'a']--;
+        }
+        return Arrays.equals(count1, count2);
+    }
+}
